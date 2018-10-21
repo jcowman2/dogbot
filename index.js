e8c3187275147830b9120c18e760296c1fc17a40
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-10-21 03:05:44 -0400

    WIP updating lambda with test api

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 /**
  * AWS Lambda Function called by GroupMe API
  */
+
+const https = require("https");
+
 exports.handler = function(event, context, callback) {
+    console.log(`Received Event.`);
+    
     const responseCode = 200;
-    const message = "Hello, world!";
+    
+    const reqOptions = {
+        host: "swapi.co",
+        path: "/api/people/1",
+        method: "GET"
+    }
+    
+    const reqGet = https.request(reqOptions, (res) => {
+        const message = JSON.stringify(res);
+        
+        const responseBody = {
+            message
+        };
 
-    const responseBody = {
-        message
-    };
-
-    const response = {
-        statusCode: responseCode,
-        body: JSON.stringify(responseBody)
-    };
-
-    callback(null, response);
+        const response = {
+            statusCode: responseCode,
+            body: JSON.stringify(responseBody)
+        };
+        
+        callback(null, response);
+    }).on("error", (e) => {
+        callback(new Error("An error occurred."))
+    })
 }
+
